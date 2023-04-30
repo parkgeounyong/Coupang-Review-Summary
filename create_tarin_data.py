e2cec7d349f96c80a2d3d24b57a261b8cfedbcd7
parkgeounyoung <50165260+parkgeounyoung@users.noreply.github.com>
2023-04-30 23:39:38 +0900

    change prompt

--- create_tarin_data.py
+++ create_tarin_data.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
         adjective_group=Tokenizer(text)
         if len(adjective_group)!=0:
             metadata.append({"prompt": "다음 형용사들은 다수의 돼지 고기 리뷰에서 추출된 형용사들이야. 해당 형용사들을 기반으로 간단한 리뷰를 작성해줘\n"
-                +str(adjective_group), "completion": text})
+                +str(adjective_group).strip('[]'), "completion": text})
 
     # 메타데이터 파일 저장
     with open("fine_truning/metadata.json", "w", encoding="utf-8") as f:
--- src/openai_method.py
+++ src/openai_method.py
@@ -5,7 +5,6 @@ from nltk import Text
 import matplotlib.pyplot as plt
 
 openai.api_key = "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"
-openai.api_base = "https://api.openai.com/v1/models/ft-G8hoYJZpI6g8cYvqJx5SQnAh"
 
 
 def Tokenizer(text):
@@ -21,7 +20,7 @@ def Tokenizer(text):
     adj_freq = Text(adj).vocab()
 
     # 빈도수가 높은 단어 10개 출력
-    top_adj = adj_freq.most_common(10)
+    top_adj = adj_freq.most_common(4)
     #top_nouns = [t[0] for t in top_nouns]
     top_adj = [t[0] for t in top_adj]
     # 결과 출력
@@ -31,18 +30,20 @@ def Tokenizer(text):
 
 def extract_element(text):
     # 문장 요약
-    prompt = "다음 형용사들은 다수의 돼지 고기 리뷰에서 추출된 형용사들이야. 해당 형용사들을 기반으로 간단한 리뷰를 작성해줘\n"
+    prompt = "$와$  사이에 있는 건 돼지 고기의 상태를 나타내는 형용사야. 해당 형용사로 후기를 작성해줘. 단, 아래의 지시사항을 참고해줘.\n\n1. 입력 언어: 한국어\n2. 출력언어: 한국어\n3. 스타일: 간결하게\n4. 독자 대상:구매자\n5. 답변 길이: utf-8 인코딩 기준 최대 100바이트\n6. 후기 카테고리: 돼지 고기\n7. 추가 지시사항:\n후기를 작성할 때 형용사는 반드시 포함하여 돼지 고기 후기를 작성해줘, 요약 형태로 작성해줘\n\n$"
     top_adj = Tokenizer(text)
-    prompt+="\n형용사: "
+    prompt+="\n"
     for token in top_adj:
         prompt += token+" "
+    prompt=prompt+"\n$"    
     completions = openai.Completion.create(
-        engine="ft-G8hoYJZpI6g8cYvqJx5SQnAh",
+        model="text-davinci-003",
         prompt=prompt,
-        max_tokens=100,
-        n=10,
-        stop=None,
-        temperature=0.8,
+        temperature=0.1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
     message = completions.choices[0].text.strip()
     return message
