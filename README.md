326f9465ed6839f5712c1acaefe09fbb3ef804f0
parkgeounyoung <50165260+parkgeounyoung@users.noreply.github.com>
2023-05-06 00:25:23 +0900

    오류 수정

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-
+#각 파일별 설명
+1. main.py
+입력: 쿠팡 url
+출력: 해당 쿠팡 url 후기 요약문
+2. create_train_data.py
+입력: 쿠팡 url
+출력: 각 후기별 
+3. create_Adjective_data.py
--- create_Adjective_data.py
+++ create_Adjective_data.py
@@ -4,23 +4,16 @@ import os
 import json
 
 #쿺팡에서 돼지고기 리뷰를 크롤링하여 토큰화
-#prompt:prompt내용+토큰, comploetion: 리뷰로 fine turning데이터 준비
 if __name__ == '__main__':
     review_collection=OpenPyXL.save_file()
 
     #with open("fine_truning/Adjective_Analysis.json", "r", encoding="utf-8") as f:
     #    metadata = json.load(f)
-    metadata = []
+    dict = {}
     
     for text in review_collection:
-        adjective_group=Tokenizer_dict(text)
-        print(adjective_group)
-        #if len(adjective_group)!=0:
-                #metadata.append({"prompt": "다음 형용사들은 다수의 돼지 고기 리뷰에서 추출된 형용사들이야. 해당 형용사들을 기반으로 간단한 리뷰를 작성해줘\n"
-                #+str(adjective_group).strip('[]'), "completion": text})
+        adjective_group=Tokenizer(text)
+        dict.update(adjective_group)
 
-    # 메타데이터 파일 저장
-    #with open("fine_truning/metadata.json", "w", encoding="utf-8") as f:
-    #    json.dump(metadata, f, ensure_ascii=False, indent=1)
     with open("fine_truning/Adjective_Analysis.json", "w", encoding="utf-8") as f:
-        json.dump(metadata, f, ensure_ascii=False, indent=1)
+        json.dump(dict, f, ensure_ascii=False, indent=1)
